17d02cd9c5606482285aaee4d467dd292899bb5b
Your Name <you@example.com>
2024-10-08 01:57:57 +0300

    changed predict function a bit

--- project.R
+++ project.R
@@ -1,5 +1,7 @@
 library(dplyr)
 library(tidytext)
+library(ggplot2)
+
 
 text_filtering <- function(file) {
   
@@ -87,26 +89,23 @@ naiveBayes <- setRefClass("naiveBayes",
                             
                             predict = function(message) {
                               words <- unlist(strsplit(message, "\\s+"))
-                              mult_lst <- list(spam = 1/.self$lens[["spam"]], ham = 1/.self$lens[["ham"]])
-                              mult <- 1/.self$lens[["len"]]
-                              product_lst <- list(spam = 1, ham = 1)
+                              product_lst <- list(spam = .self$class_probabilities[["spam"]], 
+                                                  ham = .self$class_probabilities[["ham"]])
                               
                               for (class in names(product_lst)) {
                                 for (word in words) {
                                   if (!is.null(.self$word_frequencies[[class]][[word]])) {
-                                    product_lst[[class]] <- product_lst[[class]] * .self$word_frequencies[[class]][[word]] *
-                                      .self$class_probabilities[[class]] / .self$word_frequencies[["all"]][[word]]
+                                    product_lst[[class]] <- product_lst[[class]] * .self$word_frequencies[[class]][[word]]
                                   } else {
-                                    product_lst[[class]] <- product_lst[[class]] * mult * .self$class_probabilities[[class]]/
-                                      mult_lst[[class]]
+                                    product_lst[[class]] <- product_lst[[class]] * (1 / (.self$lens[[class]] + 1))
                                   }
                                 }
                               }
                               
                               if (product_lst$spam > product_lst$ham) {
-                                return(0)
+                                return(0)  # spam
                               }
-                              return(1)
+                              return(1)  # ham
                             },
                             
                             score = function(X_test, y_test) {
@@ -122,15 +121,84 @@ naiveBayes <- setRefClass("naiveBayes",
                             }
                           ))
 
+visualize_classification_results <- function(y_true, y_pred) {
+  true_positive <- sum((y_pred == 1) & (y_true == 1))
+  false_positive <- sum((y_pred == 1) & (y_true == 0))
+  false_negative <- sum((y_pred == 0) & (y_true == 1))  # Spam classified as ham
+  true_negative <- sum((y_pred == 0) & (y_true == 0))
+  
+  results <- data.frame(
+    Result = c("True Positive", "False Positive", "False Negative", "True Negative"),
+    Count = c(true_positive, false_positive, false_negative, true_negative)
+  )
+  
+  ggplot(results, aes(x = Result, y = Count, fill = Result)) +
+    geom_bar(stat = "identity") +
+    labs(title = "Histogram of Classification Results", x = "Result Type", y = "Count") +
+    theme_minimal() +
+    scale_fill_manual(values = c("True Positive" = "blue",
+                                 "False Positive" = "red",
+                                 "False Negative" = "orange",
+                                 "True Negative" = "green"))
+}
+
+top_words <- function(filtered_text) {
+  data <- data.frame(
+    Message = unlist(filtered_text$lines),
+    Category = ifelse(filtered_text$binary_vec == 1, "ham", "spam"),
+    stringsAsFactors = FALSE
+  )
+  
+  data_words <- data %>%
+    unnest_tokens(word, Message) %>%
+    anti_join(data.frame(word = readLines("stop_words.txt"), stringsAsFactors = FALSE), by = "word")
+  
+  word_counts <- data_words %>%
+    count(Category, word, sort = TRUE) %>%
+    ungroup()
+  
+  top_words <- word_counts %>%
+    group_by(Category) %>%
+    top_n(10, n) %>%
+    arrange(Category, desc(n))
+  
+  return(top_words)
+}
+
+visualize_top_words <- function(top_words_data) {
+  ggplot(top_words_data, aes(x = reorder(word, n), y = n, fill = Category)) +
+    geom_bar(stat = "identity", position = position_dodge()) +
+    coord_flip() + 
+    labs(title = "Top 20 Words in Spam and Ham",
+         x = "Words",
+         y = "Frequency",
+         fill = "Category") +
+    theme_minimal() +
+    scale_fill_manual(values = c("spam" = "red", "ham" = "green"))
+
+}
+
 main <- function() {
   model = naiveBayes()
+  
   filtered_text <- text_filtering("./4-spam/train.csv")
   model$fit(filtered_text$lines, filtered_text$binary_vec)
   
   filtered_text1 <- text_filtering("./4-spam/test.csv")
   
-  res <- model$score(filtered_text1$lines, filtered_text1$binary_vec)
+  res <- model$score(filtered_text1$lines, filtered_text$binary_vec)
   print(res)
+  
+  y_true <- filtered_text1$binary_vec
+  y_pred <- sapply(filtered_text1$lines, model$predict)
+  
+  top_words_result <- top_words(filtered_text)
+  
+  results_df <- data.frame(True = y_true, Predicted = y_pred)
+  print(results_df)
+  
+  #visualize_top_words(top_words_result)
+  visualize_classification_results(y_true, y_pred)
 }
 
 main()
